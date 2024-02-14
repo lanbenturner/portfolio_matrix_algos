16ad9d635481d00d72c798c1eb5c8715a43da137
lanbenturner <141177007+lanbenturner@users.noreply.github.com>
2024-02-14 13:29:29 -0700

    Converted Output from Dictionary to Tuple

--- app/algos/calc_portfolio_stats.py
+++ app/algos/calc_portfolio_stats.py
@@ -23,8 +23,8 @@ def calculate_portfolio_weighting(data, portfolio_equity_rounded):
     # Extract asset data from the input JSON
     assets = data.get('assets', [])
 
-    # Initialize dictionary to store asset weightings
-    asset_weightings = {}
+    # Initialize list to store asset weightings as tuples
+    asset_weightings = []
 
     # Calculate Portfolio Weighting for each asset
     for asset in assets:
@@ -36,8 +36,8 @@ def calculate_portfolio_weighting(data, portfolio_equity_rounded):
         else:
             asset_weighting = 0  # Set weighting to 0 if portfolio equity is 0
         
-        # Store asset weighting in the dictionary
-        asset_weightings[symbol] = asset_weighting
+        # Store asset weighting as a tuple (symbol, weighting)
+        asset_weightings.append((symbol, asset_weighting))
 
     return asset_weightings
 
@@ -45,66 +45,3 @@ def calculate_portfolio_weighting(data, portfolio_equity_rounded):
 
 
 
-
-
-
-
-"""
-Test JSON:
-<COPY STARTING AT BRACKET BELOW>
-{
-    "assets": [
-        {
-            "symbol": "AAPL",
-            "equity": 5000,
-            "group_assignment": "Group1"
-        },
-        {
-            "symbol": "GOOGL",
-            "equity": 3000,
-            "group_assignment": "Group1"
-        },
-        {
-            "symbol": "MSFT",
-            "equity": 4000,
-            "group_assignment": "Group2"
-        },
-        {
-            "symbol": "AMZN",
-            "equity": 6000,
-            "group_assignment": "Group2"
-        },
-        {
-            "symbol": "FB",
-            "equity": 3500,
-            "group_assignment": "Group3"
-        },
-        {
-            "symbol": "TSLA",
-            "equity": 4500,
-            "group_assignment": "Group3"
-        },
-        {
-            "symbol": "NVDA",
-            "equity": 2000,
-            "group_assignment": "Group1"
-        },
-        {
-            "symbol": "JPM",
-            "equity": 3800,
-            "group_assignment": "Group2"
-        },
-        {
-            "symbol": "NFLX",
-            "equity": 4200,
-            "group_assignment": "Group3"
-        },
-        {
-            "symbol": "V",
-            "equity": 3500,
-            "group_assignment": "Group1"
-        }
-    ]
-}
-<COPY UNTIL BRACKET ABOVE>
-"""
